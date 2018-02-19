9e9d8ee5d04491cef85798311be8350e661bdd85
Simon Vadée <simon@stratumn.com>
2018-02-19 17:48:17 +0100

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -9,39 +9,37 @@ const globals = require("rollup-plugin-node-globals");
 const config = {
   sourcemap: true,
   input: "index.js",
-  name: "TestLibP2P"
-};
-
-config.plugins = [
-  json(),
-  builtins(),
-  commonjs(),
-  globals(),
-  babel(
-    Object.assign(
-      {
-        include: [
-          "node_modules/libp2p-crypto/**",
-          "node_modules/libp2p-crypto-secp256k1/**",
-          "node_modules/asn1.js/**",
-          "node_modules/pem-jwk/**",
-          "node_modules/multihashing-async/**",
-          "node_modules/multihashes/**",
-          "src/**"
-        ]
-      },
-      babelrc()
-    )
-  ),
-  nodeResolve({
-    browser: true,
-    preferBuiltins: true
-  })
-];
-
-config.output = {
-  format: "umd",
-  file: "dist/index.js"
+  name: "TestLibP2P",
+  plugins: [
+    json(),
+    nodeResolve({
+      browser: true,
+      preferBuiltins: true
+    }),
+    builtins(),
+    commonjs(),
+    babel(
+      Object.assign(
+        {
+          include: [
+            "node_modules/libp2p-crypto/**",
+            "node_modules/libp2p-crypto-secp256k1/**",
+            "node_modules/asn1.js/**",
+            "node_modules/pem-jwk/**",
+            "node_modules/multihashing-async/**",
+            "node_modules/multihashes/**",
+            "src/**"
+          ]
+        },
+        babelrc()
+      )
+    ),
+    globals()
+  ],
+  output: {
+    format: "umd",
+    file: "dist/index.js"
+  }
 };
 
 module.exports = config;
